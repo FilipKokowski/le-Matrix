68484dab0db53797b69aea01a91ef75424a04ec0
FilipKokowski <filipkokowski999@gmail.com>
2024-02-29 09:32:40 +0100

    Added ability to pick colors and apply them to the tiles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import {useState} from 'react';
 import './App.css';
 
+let color = 'rgb(255,0,0)';
 
-function Tile({size}){
+function Tile({size, text}){
 
-  let firstColor = 'rgb(255,0,0)';
-  let secondaryColor = 'rgb(255,255,0)';
 
   const [currentColor, setBgColor] = useState('rgb(255,0,0)');
   const changeColor = () => {
-    setBgColor(currentColor === firstColor ? secondaryColor : firstColor);
+    setBgColor(color);
   }
 
   return(
-    <div onClick={changeColor} style={{width: size, height: size, backgroundColor: currentColor, borderRadius: size / 10, float: 'left', border: (size / 25) + 'px solid rgb(128,0,0)', boxSizing: 'border-box'}}></div>
+    <div onClick={changeColor} style={{width: size, height: size, backgroundColor: currentColor, borderRadius: size / 10, float: 'left', border: (size / 25) + 'px solid rgb(128,0,0)', boxSizing: 'border-box'}}>{text}</div>
+  );
+}
+
+function ColorPicker(){
+  const [currentColor, setBgColor] = useState(color);
+  const changeColor = () => {
+    setBgColor('rgb(' + document.getElementById('red').value + ', ' + document.getElementById('green').value + ', ' + document.getElementById('blue').value + ')');
+  }
+
+  color = currentColor
+  
+  return(
+    <div>
+      <input onChange={changeColor} type='range' min='0' max='255' defaultValue='128' id='red' style={{accentColor: 'red'}}></input><br/>
+      <input onChange={changeColor} type='range' min='0' max='255' defaultValue='128' id='green' style={{accentColor: 'green'}}></input><br/>
+      <input onChange={changeColor} type='range' min='0' max='255' defaultValue='128' id='blue' style={{accentColor: 'blue'}}></input>
+      <div style={{width: '10vw', height: '10vw', backgroundColor: currentColor}}></div>
+    </div>
   );
 }
 
@@ -34,13 +51,14 @@ export default function mainPage(){
 
   for(let row = 0; row < Math.sqrt(tileNum); row++){
     for(let tile = 0; tile < Math.sqrt(tileNum); tile++){
-      tiles.push(<Tile size={tileSize}/>)
+      tiles.push(<Tile size={tileSize} text={1 + row * Math.sqrt(tileNum) + tile}/>)
     }
   }
 
   return(
     <div style={{width: tileSize * Math.sqrt(tileNum), margin: '0 auto'}}>
       {tiles}
+      <ColorPicker/>
     </div>
   );
 }
