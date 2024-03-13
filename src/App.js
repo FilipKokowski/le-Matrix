471bef2432a8b318fe9a5f3c1300f87f40374c77
Filip Kokowski <filipkokowski999@gmail.com>
2024-03-14 00:23:45 +0100

    Working on main page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import {useState} from 'react';
+import { GiHamburgerMenu  } from "react-icons/gi";
+
+import {useState, useRef} from 'react';
 import './App.css';
+
+let tiles = [];
+let tilesColors = {};
 
 let color = 'rgb(255,0,0)';
 
 function Tile({size, text}){
 
-
   const [currentColor, setBgColor] = useState('rgb(255,0,0)');
   const changeColor = () => {
     setBgColor(color);
+    tilesColors[text - 1] = color;
   }
 
   return(
@@ -22,31 +27,29 @@ function ColorPicker(){
     setBgColor('rgb(' + document.getElementById('red').value + ', ' + document.getElementById('green').value + ', ' + document.getElementById('blue').value + ')');
   }
 
-  color = currentColor
+  color = currentColor;
   
   return(
     <div>
-      <input onChange={changeColor} type='range' min='0' max='255' defaultValue='128' id='red' style={{accentColor: 'red'}}></input><br/>
-      <input onChange={changeColor} type='range' min='0' max='255' defaultValue='128' id='green' style={{accentColor: 'green'}}></input><br/>
-      <input onChange={changeColor} type='range' min='0' max='255' defaultValue='128' id='blue' style={{accentColor: 'blue'}}></input>
+      <input onChange={changeColor} type='range' min='0' max='255'  id='red' style={{accentColor: 'red'}}></input><br/>
+      <input onChange={changeColor} type='range' min='0' max='255' id='green' style={{accentColor: 'green'}}></input><br/>
+      <input onChange={changeColor} type='range' min='0' max='255'  id='blue' style={{accentColor: 'blue'}}></input>
       <div style={{width: '10vw', height: '10vw', backgroundColor: currentColor}}></div>
     </div>
   );
 }
 
+function Button({width, height, backgroundColor, color, text}){
+  return (
+    <button onClick={() => {console.log('clicked')}}style={{width: width, height: height, color: color, backgroundColor: backgroundColor, border: 'none', borderRadius: '1.5vh', fontSize: '2.25vh', fontWeight: 'bold'}}>{text}</button>
+  );
+}
+
 export default function mainPage(){
 
-  console.log(window.innerWidth);
-
-  let tiles = [];
+  tiles = [];
   let tileNum = 121;
-
   let tileSize = window.innerWidth / tileNum * (Math.sqrt(tileNum) * (9/10));
-  
-  console.log(2 * tileSize);
-
-
-  console.log((window.innerWidth - 2 * tileSize) / 5);
 
 
   for(let row = 0; row < Math.sqrt(tileNum); row++){
@@ -56,9 +59,23 @@ export default function mainPage(){
   }
 
   return(
-    <div style={{width: tileSize * Math.sqrt(tileNum), margin: '0 auto'}}>
-      {tiles}
+    /*<div style={{width: tileSize * Math.sqrt(tileNum), margin: '0 auto'}}>
+      <div>{tiles}</div>
       <ColorPicker/>
+      <button onClick={() => sendMatrix()}>Send</button>
+    </div>*/
+    <div style={{width: '100vw', height: '100vh', backgroundColor: '#F8B47F', overflow: 'hidden'}}>
+      <div className="mainTopPanel">
+        <div style={{width: '100vw', height: '2.5vh', paddingTop: '2.5vh', paddingLeft: '2.5vw'}}>
+         <GiHamburgerMenu size={'3vh'}/>
+        </div>
+        <h1 style={{margin: '1vh 0 0 2.5vw'}}>Hello There!</h1>
+      </div>
+      <div className="mainBottomPanel" style={{display: 'block'}}>
+        <div style={{width: '100vw', display: "flex", justifyContent: 'center', position: 'absolute', bottom: '5vh'}}>
+          <Button width='30vh' height='6vh' backgroundColor='#BD7F4D' color='#451800' text='Add new matrix'/>
+        </div>
+      </div>
     </div>
   );
 }
